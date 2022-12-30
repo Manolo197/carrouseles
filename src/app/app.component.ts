8239904fe8fdd379f8296e42d724ef2b094f0c6b
Manolo197 <marcelin197@hotmail.com>
2022-12-30 11:09:25 -0600

    Modelados de carrouseles sencillos (Y uno que otro avanzado)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'carrousel';
+  card:any=0;
+
+  cartas:any = [1,2,3,1,2,3,4,1,2,3,4,1,2,3,4,1,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3,1,2,3];
+  intervalo:any;
+  expand=false;
+
+
+  @ViewChild('cardsContainer') cardsContainer!:ElementRef;
+  
+  ngOnInit()
+  {
+    console.log(this.cartas.length);
+    this.setCardAnimation();
+  }
+
+  setCardAnimation()
+  {
+    this.intervalo = setInterval(()=>
+    {
+      if(this.card === (this.cartas.length-1))
+      {
+        this.showCard(0);
+      }
+      else
+      {
+        this.showCard((this.card+1));
+      }
+
+
+    }, 4000);
+  }
+
+  resetAnimaton()
+  {
+    // this.expand= false;
+    // this.showCard(this.card)
+    // this.setCardAnimation();
+  }
+
+  showCard(card:any)
+  {
+    if(this.card!=card)
+    {
+      this.card=card;
+    }
+    else if(this.card == card && !this.expand)
+    {
+      clearInterval(this.intervalo);
+      this.expand = true;
+    }
+    else if(this.card == card && this.expand)
+    {
+      this.expand = false;
+      this.setCardAnimation()
+    }
+  }
+
+
+
 }
